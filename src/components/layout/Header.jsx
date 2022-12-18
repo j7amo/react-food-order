d55f2f8180a2cbb05ceb175b373f858213c084de
Roman Tarnakin <roman_tarnakin@epam.com>
2022-12-18 16:59:23 +0300

    add header, cart button, main image

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Header.module.css';
+// one of the approaches of using images is just to import it as an object
+// and assign to 'src' attribute of <img> element
+import mealsImage from '../../assets/meals.jpeg';
+import Button from '../shared/Button';
+
+function Header() {
+  const cartOpenHandler = () => {
+    console.log('open cart');
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <Button type="button" onClick={cartOpenHandler}>
+          Open Cart
+        </Button>
+      </header>
+      <div className={styles.mainImage}>
+        <img src={mealsImage} alt="A table full of meals" />
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import styles from './Button.module.css';
+import CartIcon from '../feature/Cart/CartIcon/CartIcon';
+
+function Button({ type, onClick, children }) {
+  return (
+    <button className={styles.button} type={type} onClick={onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>{children}</span>
+      <span className={styles.badge}>3</span>
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+};
+
+export default Button;
