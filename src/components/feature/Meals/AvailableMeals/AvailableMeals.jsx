2f66a19147b086d230838a54fbbaabacfe3139d9
Roman Tarnakin <roman_tarnakin@epam.com>
2022-12-25 14:42:54 +0300

    remove hardcoded data and start fetching from firebase

--- src/components/feature/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/feature/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,52 +1,55 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './AvailableMeals.module.css';
 import MealItem from '../MealItem/MealItem';
 import Card from '../../../shared/Card/Card';
+import useFetch from '../../../../hooks/use-fetch';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+const requestConfig = {
+  url: 'https://react-movies-b2487-default-rtdb.europe-west1.firebasedatabase.app/meals.json',
+};
 
 function AvailableMeals() {
+  const [meals, setMeals] = useState([]);
+
+  const { sendRequest, isLoading, error } = useFetch();
+
+  const applyData = useCallback((data) => {
+    const transformedData = Object.entries(data).map(
+      ([id, { name, description, price }]) => ({
+        id,
+        name,
+        description,
+        price,
+      }),
+    );
+
+    setMeals(transformedData);
+  }, []);
+
+  useEffect(() => {
+    sendRequest(requestConfig, applyData);
+  }, [applyData, sendRequest]);
+
   return (
     <section className={styles.meals}>
       <Card>
-        <ul>
-          {DUMMY_MEALS.map(({
-            id, name, description, price,
-          }) => (
-            <MealItem
-              key={id}
-              id={id}
-              description={description}
-              price={price}
-              name={name}
-            />
-          ))}
-        </ul>
+        {isLoading && <p className={styles.loading}>Your meals are loading</p>}
+        {error && <p className={styles.error}>{error}</p>}
+        {!isLoading && !error && (
+          <ul>
+            {meals.map(({
+              id, name, description, price,
+            }) => (
+              <MealItem
+                key={id}
+                id={id}
+                description={description}
+                price={price}
+                name={name}
+              />
+            ))}
+          </ul>
+        )}
       </Card>
     </section>
   );
